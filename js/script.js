383d6e1915ffb28ef671119d259dd0ad617ba655
Dariia Paliy <dariia.palij@gmail.com>
2023-04-25 14:47:35 +0300

    updated html & added gallery

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// ------------MASKED PHONE-----------
+
+$("#phone").mask('+38(099) 999-99-99');
+
+$.fn.setCursorPosition = function(pos) {
+  if ($(this).get(0).setSelectionRange) {
+    $(this).get(0).setSelectionRange(pos, pos);
+  } else if ($(this).get(0).createTextRange) {
+    var range = $(this).get(0).createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+};
+
+
+$('input[type="tel"]').click(function(){
+    $(this).setCursorPosition(5);
+});
+
+
+
+// ------------GALLERY-----------
+
+$(".carousel").owlCarousel({
+  margin: 10,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
